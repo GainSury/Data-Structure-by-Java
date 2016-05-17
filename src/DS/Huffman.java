ff5d63a1c7d1525f9acbcebdbb622e3f2a36e52a
unknown <>
2016-05-17 16:48:36 +0800

    sort data completed

--- src/DS/Huffman.java
+++ src/DS/Huffman.java
@@ -1,20 +1,20 @@
 package DS;
 
-class Huffman
+class CodeTable
 {
 
 	int num;
 	char[] character;
-	double[] weigh;
+	double[] weight;
 	String[] code;
-	public Huffman(char[] character_,double[] weigh_){
+	CodeTable(char[] character_,double[] weight_){
 		num = character_.length;
 		this.character = new char[num];
-		this.weigh = new double[num];
+		this.weight = new double[num];
 		this.code = new String[num];
 		for(int i =0;i < num;i++)
 		{
-			this.weigh[i] = weigh_[i];
+			this.weight[i] = weight_[i];
 			this.character[i] = character[i];
 			
 		}
@@ -26,8 +26,52 @@ class Huffman
 		for(int i =0;i < num;i++)
 			strb = strb.append(String.format(" %-7c    %9.1f     %s\n", 
 					                         character[i],
-					                         weigh[i],
+					                         weight[i],
 					                         code[i]));
 		return new String(strb);
 	}
+}
+
+
+class HfNode
+{
+	public double weight;
+	public int parent,lchild,rchild;
+	public HfNode(double weight_,int parent_,int lchild_,int rchild_)
+	{
+		weight = weight_;
+		parent = parent_;
+		lchild = lchild_;
+		rchild = rchild_;
+		
+	}
+	
+	public String toString()
+	{
+		String str;
+		str = String.format("%5.1f    %3d    %3d   %3d", 
+				            weight,parent,lchild,rchild);
+		return str;
+	}
+}
+
+
+
+public class Huffman
+{
+	private int symbNum,nodeTotalNum;
+	private HfNode[] hfNodeTable;
+	private CodeTable codeTable;
+	Huffman(char[] character_, double[] weight_)
+	{
+        symbNum = character_.length;
+        nodeTotalNum = symbNum * 2 - 1;
+        codeTable = new CodeTable(character_, weight_);
+        hfNodeTable = new HfNode[nodeTotalNum];
+        for(int i = 0; i < nodeTotalNum; i++)
+        {
+        	hfNodeTable[i] = new HfNode(0,-1,-1,-1);
+        }
+	}
+	
 }
